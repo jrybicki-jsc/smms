fb361a770e0ee77cad3de13f1ab08a057a4cd267
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-11-03 11:22:37 +0100

    par

--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from dataprep import mysample, partition_dataframe
+from dataprep import mysample, partition_dataframe, get_part_indexes
 
 
 
@@ -24,3 +24,14 @@ class DataPrepTests(unittest.TestCase):
         n_parts = 2
         ptrs = partition_dataframe(df=df, n_parts=n_parts)
         self.assertEqual(n_parts, len(ptrs))
+
+    def test_get_part_indexes(self):
+        df = pd.DataFrame(
+            data={
+                'apn': list(range(100)),
+                'nf': list(range(100))
+            })
+        ret = get_part_indexes(dfs=df, num_parts=5, size=10)
+        self.assertEqual(len(ret), 5)
+        self.assertEquals(len(ret[0]), 10)
+
