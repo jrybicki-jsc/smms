8830402d35a5e08fe18bbdbecba07a39208032c3
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-04-25 09:29:16 +0200

    sequenciatl

--- graphla/big_converter.py
+++ graphla/big_converter.py
@@ -4,21 +4,11 @@ import numpy as np
 #import graphlab as tc
 import turicreate as tc
 import argparse
+import glob 
+import os
 
-
-
-if __name__ =="__main__":
-    parser = argparse.ArgumentParser(description='Convert apk file.')
-    parser.add_argument('--input', help='name of the input file', required=True)
-    parser.add_argument('--output', help='name of the output', required=True)
-    #fname = '../data/sample_10000_vt_mal_2017_2020_az_2020_benign_hashed_md5.csv'
-    args = parser.parse_args()
-    print(args)
-
-
-    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY',5*2147483648)
-    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY_PER_FILE', 5*134217728)
-    mw = tc.SFrame.read_csv(args.input, header=False, verbose=True)#, nrows=100)
+def convert_csv(fname, output):
+    mw = tc.SFrame.read_csv(fname, header=False, verbose=True)#, nrows=100)
     print(mw.head())
 
     mw['hapk'] = mw['X1'].apply(lambda x: hash(str.upper(x)))
@@ -26,8 +16,31 @@ if __name__ =="__main__":
     clk = mw.remove_columns(['X1', 'X2', 'X3'])
     print(clk.head())
 
-    clk.save(args.output, format='binary')
+    clk.save(output, format='binary')
 
+
+
+if __name__ =="__main__":
+    parser = argparse.ArgumentParser(description='Convert apk file.')
+    parser.add_argument('--input', help='name of the input path', required=True)
+    parser.add_argument('--output', help='output path', required=True)
+    #fname = '../data/sample_10000_vt_mal_2017_2020_az_2020_benign_hashed_md5.csv'
+    args = parser.parse_args()
+    print(args)
+
+    for f in glob.glob(args.input+'*'):
+        outnn = f.split('/')[-1]
+        outnn = os.path.join(args.output, outnn)
+        print(f"Processing {f} -> {outnn}")
+        
+        convert_csv(fname=f, output=outnn)
+
+
+    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY',5*2147483648)
+    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY_PER_FILE', 5*134217728)
+    # following can reduce the memory footprint
+    #tc.config.set_runtime_config('TURI_DEFAULT_NUM_PYLAMBDA_WORKERS', 24)
+    
     #alternative: using pandas
     #tp = pd.read_csv('large_dataset.csv', iterator=True, chunksize=1000)  # gives TextFileReader, which is iterable with chunks of 1000 rows.
     #df = pd.concat(tp, ignore_index=True)
