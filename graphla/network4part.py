408293f440fe4f9aedf1300577a3b90e031f72a0
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-05-01 18:37:00 +0200

    for arrays

--- graphla/network4part.py
+++ graphla/network4part.py
@@ -13,11 +13,13 @@ from grapm import partition_ndframe
 import datetime
 from grapm import f_create_network, convert_to_voting, save_nets
 import logging
+from streamed import get_anchor_coords
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Calculate network for a partitions')
-    parser.add_argument('--functions', help='name of the input functions', required=True)
+    parser.add_argument('--functions', help='name of the functions directory', required=True)
+    parser.add_argument('--p', help='partition number', type=int, required=True )
     parser.add_argument('--labels', help='location of labels file', required=True)
     parser.add_argument('--output', help='output path', required=True)
     parser.add_argument('--gamma', help='gamma', default=.65, type=float)
@@ -35,8 +37,8 @@ if __name__=="__main__":
     # following can reduce the memory footprint
     tc.config.set_runtime_config('TURI_DEFAULT_NUM_PYLAMBDA_WORKERS', 1)
 
-    logging.info(f"Loading functions from {args.functions}")
-    mw = tc.load_sframe(args.functions)
+    logging.info(f"Loading functions from {args.functions}{args.p}")
+    mw = tc.load_sframe(f"{args.functions}{args.p}")
     if 'fcount' in mw.column_names():
         mw.remove_column('fcount', inplace=True)
 
@@ -50,12 +52,16 @@ if __name__=="__main__":
     labels = pd.read_csv(args.labels, index_col=0)
     classifier = lambda x: int(labels.loc[x]['malware_label'])
 
-
-
     napks = mw['apk'].unique().to_numpy()
     logging.info(f"Got: {napks.shape[0]} apks")
     
     net = f_create_network(data=mw, gamma=args.gamma)
     #voting = convert_to_voting(net, classifier)
+    save_nets({args.gamma: [net]}, f"{args.gamma}-{args.p}-tc-nets",  directory=ww)
+    logging.info(f"Network with {len(net)} anchors saved ")
 
-    save_nets({args.gamma: [net]}, f"{args.gamma}-tc-nets",  directory=ww)
+    anchors = get_anchor_coords(net=net, data=mw)
+    pp = os.path.join(ww, f"anchors-{args.p}")
+    anchors.save(pp, format='binary')
+    logging.info(f"Anchor cords saved in {pp}")
+
