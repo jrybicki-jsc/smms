ac6238081ebdc886e5127947265f016a569083cf
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-05-18 09:14:32 +0200

    simpler votig

--- graphla/convert2voting.py
+++ graphla/convert2voting.py
@@ -5,13 +5,12 @@ import pandas as pd
 
 import turicreate as tc
 
-from utils import setup_path, setup_logging, load_functions_partition
+from utils import setup_path, setup_logging, setup_turi
 from grapm import convert_to_voting, save_nets
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Convert to voting network')
-    parser.add_argument('--nets', help='networks directory', required=True)
-    parser.add_argument('--p', help='partition number', type=int, required=True)
+    parser.add_argument('--net', help='networks directory', required=True)
     parser.add_argument('--labels', help='apk labels', required=True)
     parser.add_argument('--output', help='output path', required=True)
     args = parser.parse_args()
@@ -19,23 +18,20 @@ if __name__=="__main__":
     path = setup_path(args)
     setup_logging(path=path, parser=parser)
 
-    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY',5*2147483648)
-    tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY_PER_FILE', 5*134217728)
-    # following can reduce the memory footprint
-    tc.config.set_runtime_config('TURI_DEFAULT_NUM_PYLAMBDA_WORKERS', 4)
+    setup_turi()
 
     logging.info(f"Reading labels from {args.labels}")
     labels = pd.read_csv(args.labels, index_col=0)
     classifier = lambda x: int(labels.loc[x]['malware_label'])
 
     #0.86-0-tc-nets.pickle
-    with open(f"{args.nets}{args.p}-tc-nets.pickle", 'rb') as f:
+    with open(f"{args.net}", 'rb') as f:
         net = pickle.load(f)
     
     gamma = list(net.keys())[0]
     net = list(net.values())[0][0]
     voting = convert_to_voting(net, classifier)
     # args.nets will include path so we trick it with directory =''
-    save_nets({gamma: [voting]}, f"{args.nets}-{args.p}-voting-net",  directory='')
+    voting_path = args.net.replace('.pickle', '-voting')
+    save_nets({gamma: [voting]}, voting_path,  directory='')
     logging.info(f"Voting network with {len(voting)} anchors saved ")
-    
