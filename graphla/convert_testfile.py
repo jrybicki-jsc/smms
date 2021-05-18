fecb3158b1f87f00b49739f9416ee640c34ebef2
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-05-18 16:00:25 +0200

    convert and netw

--- graphla/convert_testfile.py
+++ graphla/convert_testfile.py
@@ -1,13 +1,24 @@
 import numpy as np 
-import turicreate as tc 
+import turicreate as tc
+import pickle
 
 if __name__=="__main__":
     test_file = ''
     functions = ''
+    net_file =''
 
-    print(f"Reading test file: {test_file}")
-    test_apns = np.load(test_file)
-    
+    if test_file:
+        print(f"Reading test file: {test_file}")
+        test_apns = np.load(test_file)
+    else:
+        print(f"Reading reading net file {net_file}")
+        with open(net_file, 'rb') as f:
+            net = pickle.load(f)
+        net = list(net.values())[0][0]
+        test_apns = list(net.keys())
+
+    print(f"Test apn: {test_apns}")
+
     tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY',15*2147483648)
     tc.config.set_runtime_config('TURI_FILEIO_MAXIMUM_CACHE_CAPACITY_PER_FILE', 15*134217728)
     # following can reduce the memory footprint
