e0d142ec1539460dc2bcd3c0125004f9c68bd435
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-05-21 14:58:34 +0200

    net_stat

---
+++ graphla/net_stat.py
@@ -0,0 +1,14 @@
+#print nets stats
+import glob
+from utils import load_net
+
+if __name__=="__main__":
+    
+    for fname in glob.glob('*.pickle'):
+        try:
+            g2, net = load_net(fname)
+            print(f"{fname} gamma={g2} size: {len(net)}")
+        finally:
+            print(f"Unable to load net from {fname}")
+
+        
