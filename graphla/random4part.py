9167529c4a4cec34899c36f1888f6fbffd36145c
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-06-10 20:59:08 +0200

    random instead of net

---
+++ graphla/random4part.py
@@ -0,0 +1,41 @@
+# spliter
+import argparse
+import logging
+import os
+
+import numpy as np
+
+from grapm import save_nets
+from streamed import get_anchor_coords
+from utils import (load_functions_partition, setup_logging, setup_path,
+                   setup_turi)
+
+
+def random_net(data, size):
+    apks = data['apk'].unique()
+    return {a:[] for a in np.random.choice(a=apks, size=size, replace=False)}
+
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Calculate random set from a partition')
+    parser.add_argument('--functions', help='name of the functions directory', required=True)
+    parser.add_argument('--p', help='partition number', type=int)
+    parser.add_argument('--output', help='output path', required=True)
+    parser.add_argument('--size', help='size', default=120, type=int)
+    args = parser.parse_args()
+
+    path = setup_path(args=args)
+    setup_logging(path=path, parser=parser)
+    setup_turi()
+
+    mw = load_functions_partition(directory=args.functions, name=args.p)
+    logging.info(f"Stargng network creation for size={args.size}")
+    net = random_net(data=mw, size=args.size)
+    save_nets({args.size: [net]}, f"{args.size}-{args.p}-random-nets",  directory=path)
+    logging.info(f"Network with {len(net)} anchors saved ")
+
+    anchors = get_anchor_coords(net=net, data=mw)
+    pp = os.path.join(path, f"anchors-{args.p}")
+    anchors.save(pp, format='binary')
+    logging.info(f"Anchor cords saved in {pp}")
