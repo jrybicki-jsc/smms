29b4c0acd5e0b102378341ab55e6269be313cf06
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-08-26 09:48:37 +0200

    merger with anchors

--- graphla/net_merger.py
+++ graphla/net_merger.py
@@ -11,8 +11,9 @@ from tqdm import tqdm
 
 import turicreate as tc
 from grapm import f_create_network, save_nets
+from streamed import get_anchor_coords
 from utils import (load_functions_partition, setup_logging, setup_path,
-                   setup_turi)
+                   setup_turi, load_net)
 
 
 def merge_voting_nets(nets, datas, gamma):
@@ -43,14 +44,13 @@ def merge_voting_nets(nets, datas, gamma):
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Merge networks')
     parser.add_argument('--nets', help='name of the network directory', required=True)
-    #parser.add_argument('--anchors', hep='name of ')
     parser.add_argument('--p1', help='partition number', type=int, required=True)
     parser.add_argument('--p2', help='partition number', type=int, required=True)
     parser.add_argument('--output', help='output path', required=True)
     parser.add_argument('--gamma', help='gamma', default=.65, type=float)
     args = parser.parse_args()
 
-    path = setup_path(args=args)
+    path = setup_path(args=args, time=False)
     setup_logging(path=path, parser=parser)
     setup_turi()
 
@@ -68,10 +68,8 @@ if __name__=="__main__":
 
     for i in range(args.p1, args.p2+1):
         #0.85-0-tc-nets-voting.pickle
-        with open(os.path.join(args.nets, f"{gamma}-{i}-tc-nets-voting.pickle"), 'rb') as f:
-            net = pickle.load(f)
-        networks.append(list(net.values())[0][0])
-        g2 = list(net.keys())[0]
+        g2, net = load_net(os.path.join(args.nets, f"{gamma}-{i}-tc-nets-voting.pickle"))
+        networks.append(net)
         if g2!=gamma:
             logging.warning(f"Found different gamman in network file {i}: {gamma}!={g2}")
             gamma =g2 
@@ -82,5 +80,12 @@ if __name__=="__main__":
 
     logging.info(f"Starting to merge {len(networks)} nets with gamma={gamma}")
     r = merge_voting_nets(nets=networks, datas=anchors, gamma=gamma)
-    save_nets({gamma: [r]}, f"merged-{gamma}-{args.origin}-voting",  directory=path)
+    save_nets({gamma: [r]}, f"merged-{gamma}-0-voting",  directory=path)
     logging.info(f"Saved network with {len(r)}")
+
+    anchors = get_anchor_coords(net=r, data=anchors)
+    pp = os.path.join(path, f"anchors-{gamma}-0")
+    anchors.save(pp, format='binary')
+    logging.info(f"Anchor cords saved in {pp}")
+
+    
